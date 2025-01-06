96ddaae70bea66b0a01b3756a0eeafec641c6618
arman <armanar6712@gmail.com>
2025-01-06 22:13:51 +0530

    Feat: fetch long url from database and redirect to long url

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import Auth from "./auth";
 import Dashboard from "./dashboard";
 import LandingPage from "./landing";
 import LinkPage from "./link";
-import RedirectLink from "./redirect";
+import RedirectLink from "./redirect-link";
 
 export { Auth, Dashboard, LandingPage, LinkPage, RedirectLink };
